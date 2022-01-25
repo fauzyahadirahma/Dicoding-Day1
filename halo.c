a8561fc89d94af2cb4989fd156e59241489ad203
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:36:37 +0700

    Menulis sebuah string, 26 Desember 2021

---
+++ halo.c
@@ -0,0 +1,11 @@
+/* File : halo.c */
+/* Penulis : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com */
+/* Program menulis "Halo Dunia!" dan "end-of-line" (ganti baris) */
+
+#include <stdio.h>
+
+int main(void) {
+    /* Badan Program */
+    printf("Hello Dunia!\n");
+    return 0;
+}
